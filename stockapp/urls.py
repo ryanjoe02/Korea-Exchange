8b000961c203eb98530d5963b5ecfc9a569c1352
syjoe <csy5501@gmail.com>
2024-08-19 19:14:24 +0900

    ADD: top3 close price

--- stockapp/urls.py
+++ stockapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import latest_kospi_data
+from .views import latest_kospi_data, top3_close_price
 
 urlpatterns = [
     path('api/latest/', latest_kospi_data, name='latest_kospi_data'),
+    path('api/top3-close-price', top3_close_price, name='top3_close_price'),
 ]
--- stockapp/views.py
+++ stockapp/views.py
@@ -1,14 +1,31 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from datetime import datetime, timedelta
 
 from .models import KospiData
 from .serializers import KospiDataSerializer
-from datetime import datetime, timedelta
 
 @api_view(['GET'])
 def latest_kospi_data(request):
-    fifty_days = datetime.now().date() - timedelta(days=50)
+    fifty_days = datetime.now().date() - timedelta(days=10)
     latest_data = KospiData.objects.filter(date__gte=fifty_days).order_by('-date')
     serializer = KospiDataSerializer(latest_data, many=True)
 
     return Response(serializer.data)
+
+@api_view(['GET'])
+def top3_close_price(request):
+    top3_close_record = KospiData.objects.order_by('-close_price')[:3]
+
+    if top3_close_record:
+        response_data = [
+            {
+                'date': record.date,
+                'close_price' : record.close_price,
+            }
+            for record in top3_close_record
+        ]
+    else:
+        response_data = {'error': 'No data available'}
+    
+    return Response(response_data)
