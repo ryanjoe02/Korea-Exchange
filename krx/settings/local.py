e55726bbf49d05489437b09591afe22ae185c540
syjoe <csy5501@gmail.com>
2024-09-27 19:30:34 +0900

    ADD: while debug is True, no need auth

--- krx/settings/local.py
+++ krx/settings/local.py
@@ -1,5 +1,12 @@
 import os
 
 from .base import *
+from django.conf import settings
 
 DEBUG = True
+
+if settings.DEBUG:
+    REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] = []
+    REST_FRAMEWORK["DEFAULT_PERMISSION_CLASSES"] = [
+        "rest_framework.permissions.AllowAny"
+    ]
